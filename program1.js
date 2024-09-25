612569b3eec6042af02eba416254cc33444a045e
0506cs211007 <0506cs211007@gmail.com>
2024-09-25 00:44:31 -0700

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,9 +1,9 @@
-const dispatch2 = [
-    ["L","L","W","W","W"],
-    ["L","L","W","W","W"],
-    ["W","W","L","W","W"],
-    ["W","W","W","L","L"],
-];
+// const dispatch2 = [
+//     ["L","L","W","W","W"],
+//     ["L","L","W","W","W"],
+//     ["W","W","L","W","W"],
+//     ["W","W","W","L","L"],
+// ];
 
 const getTotalIsles = function (grid) {
 
